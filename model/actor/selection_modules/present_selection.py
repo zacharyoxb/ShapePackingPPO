5ac402583d07432eaa71c6f7ad7d3b8983b1d547
zacharyoxb <oxboroughzachary@protonmail.com>
2026-02-10 17:11:50 +0000

    removed pointless dim modifications

--- model/actor/selection_modules/present_selection.py
+++ model/actor/selection_modules/present_selection.py
@@ -45,49 +45,13 @@ class PresentSelectionActor(nn.Module):
 
         self._all_present_features = torch.stack(all_features)
 
-    def _format_data(self, tensordict):
+    def forward(self, tensordict):
+        """ Gets scores for orientation / modulated grids for them """
         grid = tensordict.get("grid")
         present_count = tensordict.get("present_count")
 
-        # If multithreaded, combine into total_batch
-        workers, batches = None, None
-        # If inputs are unbatched
-        if grid.dim() == 2:
-            # Add batch/channel
-            grid = grid.unsqueeze(0).unsqueeze(0)
-            present_count = present_count.unsqueeze(0)
-        # If inputs are single batched
-        elif grid.dim() == 3:
-            # Add channel
-            grid = grid.unsqueeze(1)
-        # If inputs are double batched
-        elif grid.dim() == 4:
-            # Add channel
-            grid = grid.unsqueeze(2)
-            # combine workers and batches
-            workers, batches = grid.shape[0], grid.shape[1]
-            grid = grid.view(workers * batches, *grid.shape[2:])
-            present_count = present_count.view(
-                workers * batches, *present_count.shape[2:])
-
+        # Extract features
         grid_features = self.grid_extractor(grid)
-
-        return grid_features, present_count, workers, batches
-
-    def _apply_film(self, grid_features, present_features, workers, batches):
-        score, modulated_grid = self.film(
-            grid_features, present_features)
-        if workers and batches:
-            modulated_grid = modulated_grid.view(
-                workers, batches, -1, -1, -1)
-
-        return score, modulated_grid
-
-    def forward(self, tensordict):
-        """ Gets scores for orientation / modulated grids for them """
-        # get data in valid format
-        grid_features, present_count, workers, batches = self._format_data(
-            tensordict)
 
         orient_logits = torch.tensor([])
         orient_data = []
@@ -98,26 +62,21 @@ class PresentSelectionActor(nn.Module):
                 continue
 
             for orient_idx, orient_features in enumerate(present_features):
-                score, modulated_grid = self._apply_film(
-                    grid_features,
-                    orient_features,
-                    workers,
-                    batches
-                )
+                score, modulated_grid = self.film(
+                    grid_features, orient_features)
+
                 orient_logits.add(score)
                 orient_data.append(
-                    TensorDict.from_dataclass(
-                        OrientationEntry(
-                            torch.tensor(present_idx),
-                            torch.tensor(orient_idx),
-                            self._all_present_features[present_idx][orient_idx],
-                            modulated_grid
-                        )
+                    OrientationEntry(
+                        torch.tensor(present_idx),
+                        torch.tensor(orient_idx),
+                        self._all_present_features[present_idx][orient_idx],
+                        modulated_grid
                     )
                 )
 
         choice_idx = torch.multinomial(orient_logits, len(orient_data))
 
         return TensorDict({
-            "present_data": orient_data[choice_idx]
+            "present_data": TensorDict.from_dataclass(orient_data[choice_idx])
         })
