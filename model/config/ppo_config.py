7c5dc57a8f8f3ecfe6a3d9c598a7c4ffc7cac5c5
zacharyoxb <oxboroughzachary@protonmail.com>
2026-06-04 20:30:01 +0100

    increased batch size / epochs after smaller nns

--- model/config/ppo_config.py
+++ model/config/ppo_config.py
@@ -34,8 +34,8 @@ class DataCollection:
 @dataclass
 class PPOParameters:
     """ Loss function weights and coefficients """
-    sub_batch_size = 32
-    num_epochs = 5
+    sub_batch_size = 64
+    num_epochs = 10
     clip_epsilon = 0.2
     gamma = 0.99
     lmbda = 0.95
